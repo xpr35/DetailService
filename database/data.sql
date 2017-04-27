b65e8c2a4cd33153fafbc8d0dc727ddb18f76425
Lev Novokreshchenov <xpr35@live.com>
2017-04-28 01:57:38 +0600

    fix db dump deploy

--- database/data.sql
+++ database/data.sql
@@ -1,3 +1,12 @@
+CREATE DATABASE DetailService;
+USE DetailService;
+CREATE TABLE `Cell` (
+  `cell_id` int(11) NOT NULL,
+  `ctn` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Cell_id_uindex` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
 INSERT INTO DetailService.Cell (cell_id, ctn) VALUES (11111, 1234567890);
 INSERT INTO DetailService.Cell (cell_id, ctn) VALUES (11111, 1234567891);
 INSERT INTO DetailService.Cell (cell_id, ctn) VALUES (11111, 1234567892);
--- database/database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE DetailService;
-USE DetailService;
-CREATE TABLE `Cell` (
-  `cell_id` int(11) NOT NULL,
-  `ctn` int(11) NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `Cell_id_uindex` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
